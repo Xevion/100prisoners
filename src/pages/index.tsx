3a6d3647e02a91f0198460202a0ebbee14918e0b
Xevion <ryanchwalters@gmail.com>
2022-12-17 23:04:36 -0600

    Add prose, adjust body scaling/spacing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,24 +10,31 @@ const Home: NextPage = () => {
                 <title>100prisoners.com</title>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
-            <main className="flex min-h-screen flex-col items-center bg-white">
-                <div className="mt-8 px-3 max-w-[50rem] w-[80%] space-y-3">
-                    <h1 className="text-4xl mb-2">100Prisoners.com</h1>
-                    <div>
-                        This website is dedicated to exploring the intriguing 100 prisoners problem, a mathematical
+            <main className="flex w-full flex-col items-center bg-white">
+                <div className="mt-8 px-3 max-w-screen-md w-[90%]">
+                    <h1 className="text-3xl sm:text-5xl mb-2">100Prisoners.com</h1>
+                    <div className="prose lg:prose-xl">
+                        <p>
+                            This website is dedicated to exploring the intriguing 100 prisoners problem, a mathematical
                         challenge that seems astronomically impossible at first, yet can leverage mathematics to raise
                         the chances one hundred octillion.
                         <br/>
                         <br/>
                         This thought experiment presents a scenario in which a group of 100 prisoners are tasked with
                         finding their own numbered slip among a collection of 100 boxes, each containing a random
-                        permutation of the numbers 1 through 100. The prisoners are allowed to open 50 boxes each in an
+                        permutation of the numbers 1 through 100.
+                        </p>
+                        <p>
+                            The prisoners are allowed to open 50 boxes each in an
                         attempt to find their own number, and all of the prisoners must be successful in order to be set
                         free. This problem raises questions about strategy and probability in search of a solution.
                     </div>
-                    <NoSSR>
-                        <BoxTable/>
-                    </NoSSR>
+                    <div className="pt-5">
+
+                        <NoSSR>
+                            <BoxTable/>
+                        </NoSSR>
+                    </div>
                 </div>
             </main>
         </>
