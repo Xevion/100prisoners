0a53c7a060aec005113a8b4d31fb034762ec44f4
Xevion <ryanchwalters@gmail.com>
2022-12-18 03:05:02 -0600

    Add GitHub link to footer with Globe icon

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -3,6 +3,8 @@ import type {FunctionComponent} from "react";
 import React from "react";
 import DarkModeSwitch from "@/components/DarkModeSwitch";
 
+import {GlobeAltIcon} from "@heroicons/react/20/solid";
+
 interface PageProps {
     children: React.ReactNode;
 }
@@ -10,8 +12,9 @@ interface PageProps {
 const Page: FunctionComponent<PageProps> = ({children}: PageProps) => {
     return (
         <>
-            <main className="flex w-full flex-col items-center bg-white dark:bg-zinc-900 text-gray-700 dark:text-gray-300">
-                <DarkModeSwitch className="fixed top-5 right-5" />
+            <main
+                className="flex w-full flex-col items-center bg-white dark:bg-zinc-900 text-gray-700 dark:text-gray-300">
+                <DarkModeSwitch className="fixed top-5 right-5"/>
                 <div className="mt-8 px-3 max-w-screen-md w-[90%]">
                     <h1 className="text-3xl font-rokkitt text-zinc-800 dark:text-zinc-100 sm:text-5xl mb-2">
                         <Link href={"/"}>100Prisoners.com</Link>
@@ -24,6 +27,9 @@ const Page: FunctionComponent<PageProps> = ({children}: PageProps) => {
                                 Created by <Link href={"https://xevion.dev"} about="_blank">
                                 <span className="font-bold">
                                     Ryan Walters
+                                    <Link href={"https://github.com/Xevion/100prisoners"} about="_blank">
+                                        <GlobeAltIcon className="w-4 h-4 mx-1.5 mb-1.5 inline text-sky-500"/>
+                                    </Link>
                                 </span>
                             </Link>
                             </div>
